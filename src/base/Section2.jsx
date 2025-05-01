9c854db4a97a5c0ad3b7ec5b299e9aa521431904
AbhiThakurL <xmrset@gmail.com>
2025-05-01 22:51:17 +0530

    Modified testing.jsx file to add a button

--- src/base/Section2.jsx
+++ src/base/Section2.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import testing from './testing'
 
 export default function Section2() {
   return (
     <div className='w-[100%] '>
       <div className="flex items-center">
+        <testing/>
+
 
 
       </div>
---
+++ src/base/testing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function testing() {
+  return (
+    <div className='text-2xl  text-red-600'>
+
+        <h1>Hello Tesing APi</h1>
+      
+    </div>
+  )
+}
